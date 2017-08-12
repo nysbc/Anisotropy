a0c67222eca0e76758a51583f6d13b841a9404a3
Carl Negro <carlnegro@gmail.com>
2017-08-12 12:52:39 -0400

    Sorry status bar, you get an equals sign instead of a crazy unicode character.

--- ThreeDFSC/programs/ThreeDFSC_Analysis_V3.py
+++ ThreeDFSC/programs/ThreeDFSC_Analysis_V3.py
@@ -59,7 +59,7 @@ def print_progress(iteration, total, prefix='', suffix='', decimals=1):
 	str_format = "{0:." + str(decimals) + "f}"
 	percents = str_format.format(100 * (iteration / float(total)))
 	filled_length = int(round(bar_length * iteration / float(total))) ## adjusted base on window size
-	bar = '█' * filled_length + '-' * (bar_length - filled_length)
+	bar = '=' * filled_length + '-' * (bar_length - filled_length)
 
 	sys.stdout.write('\x1b[2K\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
 
